e59c9074be6e0583845b90c06aeb26adcd7f6a69
Anupama <akhangar@cisco.com>
2019-11-16 21:39:54 +0530

    Service takes data from kafka and store in postgres and redis cache

---
+++ src/test/java/com/anupama/ks/producer/DataProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupama.ks.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
